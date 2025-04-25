c7a11b8c839e4408672ede8f0ff5f6c64b11b90c
binhhp <Binhhp20@gmail.com>
2025-04-25 08:37:32 +0700

    update client

---
+++ client/src/dashboard/plugins/anti-spy-plugin/plugin.tsx
@@ -0,0 +1,34 @@
+import React, { type FC } from "react";
+import type { plugins } from "@wix/bookings/dashboard";
+import {
+  WixDesignSystemProvider,
+  Card,
+  Text,
+  TextButton,
+} from "@wix/design-system";
+import "@wix/design-system/styles.global.css";
+
+type Props = plugins.BookingCalendar.ExtensionProps;
+
+const Plugin: FC<Props> = (props) => {
+  return (
+    <WixDesignSystemProvider features={{ newColorsBranding: true }}>
+      <Card>
+        <Card.Header title="Dashboard Plugin" />
+        <Card.Divider />
+        <Card.Content size="medium">
+          <Text>
+            This dashboard plugin was generated with Wix CLI. Customize it
+            according to your logic. To learn more, read our{" "}
+            <TextButton as="a" href="https://wix.to/dFFuEki" target="_blank">
+              documentation
+            </TextButton>
+            .
+          </Text>
+        </Card.Content>
+      </Card>
+    </WixDesignSystemProvider>
+  );
+};
+
+export default Plugin;
